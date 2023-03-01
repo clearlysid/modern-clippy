5b425cfa6152d9687fd971222b475a9cf1ee801a
Siddharth <siddharth99c@gmail.com>
2023-03-01 16:46:00 +0530

    chore: adds styling library and framer motion

--- src/renderer/preload.ts
+++ src/renderer/preload.ts
@@ -1,6 +1,5 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('api', {
-  askBing: (query: string) => ipcRenderer.invoke('function:askBing', query),
-	test: "hello world"
+  askBing: (query: string) => ipcRenderer.invoke('function:askBing', query)
 })
