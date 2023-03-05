18e93bb5e5f226b1fd49340d15d5664d348bf88d
Siddharth <siddharth99c@gmail.com>
2023-03-05 17:33:10 +0530

    feat: adds hider utility

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,9 +1,11 @@
 import React, { useRef, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
+import { motion } from "framer-motion";
 
 import Menu from "./components/Menu";
 import Form from "./components/Form";
 import Chat from "./components/Chat";
+import Hide from "./components/Hide";
 import initialTexts from "./components/initialTexts";
 
 import type { ChatMessage } from "./types";
@@ -13,9 +15,8 @@ const App = () => {
 
 	const askBing = useRef(null)
 
-	// const initialMessages: ChatMessage[] = []
-	const initialMessages: ChatMessage[] = initialTexts
-
+	const initialMessages: ChatMessage[] = []
+	// const initialMessages: ChatMessage[] = initialTexts
 
 	const [thinking, setThinking] = useState<boolean>(false)
 	const [lastChat, setLastChat] = useState<BingResponse | null>(null)
@@ -24,7 +25,7 @@ const App = () => {
 	const handleQuery = async (query: string) => {
 		setMessages([...messages, { type: "outgoing", data: query }])
 
-		const startThinking = setTimeout(() => setThinking(true), 1000)
+		const startThinking = setTimeout(() => setThinking(true), 2000)
 
 		let response
 
@@ -49,18 +50,21 @@ const App = () => {
 	}, [])
 
 	return (
-		<div css={{
-			padding: 24,
-			rowGap: 8,
-			width: "100%",
-			height: "100%",
-			display: "flex",
-			flexDirection: "column",
-		}}>
-			<Menu />
+		<motion.div
+			layout
+			css={{
+				rowGap: 8,
+				padding: 8,
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				flexDirection: "column",
+			}}>
+			<Hide />
 			<Chat messages={messages} thinking={thinking} />
 			<Form handleQuery={handleQuery} thinking={thinking} />
-		</div>
+			<Menu />
+		</motion.div>
 	);
 };
 
---
+++ src/renderer/components/Hide.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Hide() {
+	return <div css={{
+		flex: 1,
+	}} onClick={(e) => console.log("hide window now")} />
+}
+
