7782017103087073a3fd61ac5780062f2e8202df
Siddharth <siddharth99c@gmail.com>
2023-03-05 17:33:23 +0530

    feat: lock to one instance

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,31 +7,44 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-app.on('ready', () => {
-  ipcMain.handle('function:askBing', getBingResponse)
+let mainWindow: Electron.BrowserWindow;
 
-  app.setActivationPolicy('accessory') // macOS only
+const lock = app.requestSingleInstanceLock()
+const toggleWindow = () => {
+  if (mainWindow) {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+  }
+} 
 
-  const mainWindow = createMainWindow();
-
-  // open dev tools
-  mainWindow.webContents.openDevTools();
-
-  const toggleWindow = () => mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
-
-  const trayIcon = new Tray("assets/ClippyIconTemplate.png")
-  trayIcon.addListener('click', () => toggleWindow())
-  trayIcon.addListener('right-click', () => app.quit())
-
-  globalShortcut.register('CommandOrControl+Shift+6', () => toggleWindow())
-  globalShortcut.register('CommandOrControl+Shift+7', () => app.quit())
-  globalShortcut.register('CommandOrControl+R', function() {
-		mainWindow.reload()
-	})
-
-  mainWindow.on('blur', () => mainWindow.hide())
-});
-
+if (!lock) {
+  app.quit()
+} else {
+    // Handle second instance
+    app.on('second-instance', () => toggleWindow())
+  
+    // Create main window
+    app.on('ready', () => {
+      ipcMain.handle('function:askBing', getBingResponse)
+    
+      app.setActivationPolicy('accessory') // macOS only
+    
+      mainWindow = createMainWindow();
+    
+      // open dev tools
+      mainWindow.webContents.openDevTools();
+    
+      const trayIcon = new Tray("assets/ClippyIconTemplate.png")
+      trayIcon.addListener('click', () => toggleWindow())
+      trayIcon.addListener('right-click', () => app.quit())
+    
+      globalShortcut.register('CommandOrControl+Shift+6', () => toggleWindow())
+      globalShortcut.register('CommandOrControl+R', function() {
+        mainWindow.reload()
+      })
+    
+      // mainWindow.on('blur', () => mainWindow.hide())
+    });
+}
 
 // don't quit if all windows are closed
 app.on('window-all-closed', (e: Event) => {
