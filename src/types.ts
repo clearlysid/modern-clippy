65d1ad0ef14e90f0161bc85b217903dd9a043b5a
Siddharth <siddharth99c@gmail.com>
2023-03-08 10:48:49 +0530

    style: formatting

--- src/types.ts
+++ src/types.ts
@@ -1,70 +1,69 @@
-
 export type ChatMessage = {
-	type: 'incoming' | 'outgoing',
-	data: string | BingResponse
-}
+	type: "incoming" | "outgoing";
+	data: string | BingResponse;
+};
 
 export type BingResponse = {
-	author:       string;
+	author: string;
 	invocationId: string;
-	text:         string;
-	detail:       Detail;
-}
+	text: string;
+	detail: Detail;
+};
 
 export type Detail = {
-	text:               string;
-	author:             string;
-	createdAt:          Date;
-	timestamp:          Date;
-	messageId:          string;
-	requestId:          string;
-	offense:            string;
-	adaptiveCards:      AdaptiveCard[];
+	text: string;
+	author: string;
+	createdAt: Date;
+	timestamp: Date;
+	messageId: string;
+	requestId: string;
+	offense: string;
+	adaptiveCards: AdaptiveCard[];
 	sourceAttributions: SourceAttribution[];
-	feedback:           Feedback;
-	contentOrigin:      string;
-	privacy:            null;
+	feedback: Feedback;
+	contentOrigin: string;
+	privacy: null;
 	suggestedResponses: SuggestedResponse[];
-}
+};
 
 export type AdaptiveCard = {
-	type:    string;
+	type: string;
 	version: string;
-	body:    Body[];
-}
+	body: Body[];
+};
 
 export type Body = {
-	type:  string;
-	text:  string;
-	wrap:  boolean;
+	type: string;
+	text: string;
+	wrap: boolean;
 	size?: string;
-}
+};
 
 export type Feedback = {
-	tag:       null;
+	tag: null;
 	updatedOn: null;
-	type:      string;
-}
+	type: string;
+};
 
 export type SourceAttribution = {
 	providerDisplayName: string;
-	seeMoreUrl:          string;
-	imageLink?:          string;
-	imageWidth?:         string;
-	imageHeight?:        string;
-	imageFavicon?:       string;
-	searchQuery:         string;
-}
+	seeMoreUrl: string;
+	imageLink?: string;
+	imageWidth?: string;
+	imageHeight?: string;
+	imageFavicon?: string;
+	searchQuery: string;
+};
 
 export type SuggestedResponse = {
-	text:          string;
-	author:        string;
-	createdAt:     Date;
-	timestamp:     Date;
-	messageId:     string;
-	messageType:   string;
-	offense:       string;
-	feedback:      Feedback;
+	text: string;
+	author: string;
+	createdAt: Date;
+	timestamp: Date;
+	messageId: string;
+	messageType: string;
+	offense: string;
+	feedback: Feedback;
 	contentOrigin: string;
-	privacy:       null;
-}
+	privacy: null;
+};
